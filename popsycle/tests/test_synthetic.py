08e416e2b78bf3794c60dd66b2b0ca0dc8208bca
Jessica Lu <jlu.astro@yahoo.com>
2019-08-06 13:10:28 -0700

    Adding new test

---
+++ popsycle/tests/test_synthetic.py
@@ -0,0 +1,9 @@
+from popsycle import synthetic
+from astropy.table import Table
+import ebf
+
+def test_calc_blends():
+    test_data_dir = '/u/casey/scratch/work/microlens/galaxia_test/OGLE672/'
+
+    
+    return

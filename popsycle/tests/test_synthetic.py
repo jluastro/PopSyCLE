c785d321bfacc318b550505cc6d542f34e91c112
Casey Lam <casey_lam@berkeley.edu>
2019-08-09 09:18:24 -0700

    testing for _calc_event_time_loop and a timing test

--- popsycle/tests/test_synthetic.py
+++ popsycle/tests/test_synthetic.py
@@ -7,6 +7,10 @@ from astropy import units
 import numpy as np
 import h5py
 import pdb
+from multiprocessing import Pool
+import itertools
+import matplotlib.pyplot as plt
+import time
 
 masyr_to_degday = 1.0 * (1.0e-3 / 3600.0) * (1.0 / 365.25)
 kms_to_kpcday = 1.0 * (3.086 * 10**16)**-1 * 86400.0
@@ -61,3 +65,101 @@ def test_calc_blends():
     lens_obj_id_1 = blend_lbt[0, 0]
     
     return
+
+def time_calc_event_time_loop():
+    t1 = test_calc_event_time_loop(1)
+    t4 = test_calc_event_time_loop(4)
+    t8 = test_calc_event_time_loop(8)
+    t12 = test_calc_event_time_loop(12)
+    
+    t_proc = np.array([t1, t4, t8, t12])
+    t_thry = t_proc[0] * np.array([1, 1/4.0, 1/8.0, 1/12.0])
+    n_proc = np.array([1, 4, 8, 12])
+    
+    fig = plt.figure(1, figsize=(6,6))
+    plt.clf()
+    plt.plot(n_proc, t_thry, '.', label = 'Ideal')
+    plt.plot(n_proc, t_proc, '.', label = 'Reality')
+    plt.xlabel('N processors')
+    plt.ylabel('Time (sec)')
+    plt.legend()
+    plt.show()
+
+def test_calc_event_time_loop(n_proc):
+    t0 = time.time()
+    
+    test_data_dir = '/u/casey/scratch/work/microlens/galaxia_test/OGLE672/'
+    hdf5_file = test_data_dir + 'OGLE672.h5'
+    obs_time = 1000
+    n_obs = 101
+    radius_cut = 2
+    theta_frac = 2
+    blend_rad = 0.8
+
+    # Initialize events_tmp and blends_tmp.
+    events_tmp = None
+    blends_tmp = None
+
+    # Get the l and b from the HDF5 file.
+    hf = h5py.File(hdf5_file, 'r')
+    l_array = np.array(hf['long_bin_edges'])
+    b_array = np.array(hf['lat_bin_edges'])
+    hf.close()
+    
+    # Converts radius_cut from arcseconds into milliarcseconds
+    radius_cut *= 1000.0
+
+    # Set up the multiprocessing
+    pool = Pool(n_proc)
+
+    # Set up inputs to be able to be read by pool.map
+    # Only do a few of the bins.
+    nll = 7
+    nbb = 7
+    
+    llbb = itertools.product(range(nll), range(nbb))
+
+    reps = nll * nbb
+
+    hd = itertools.repeat(hdf5_file, reps)
+    ot = itertools.repeat(obs_time, reps)
+    no = itertools.repeat(n_obs, reps)
+    rc = itertools.repeat(radius_cut, reps)
+    tf = itertools.repeat(theta_frac, reps) 
+    br = itertools.repeat(blend_rad, reps)
+
+    inputs = zip(llbb, hd, ot, no, rc, tf, br)
+
+    ##########
+    # Loop through galactic latitude and longitude bins. For each bin vertex, take
+    # the nearest 4 bin samples and calculate microlensing events. We do this
+    # to properly handle bin edges (i.e. a sliding window analysis of 2x2 bins).
+    # Duplicate events are removed. 
+    ##########
+    # map_async? Make it wait till there are all done.
+    results = pool.starmap(synthetic._calc_event_time_loop, inputs)
+
+    pool.close()
+    pool.join()
+
+    results = [i for i in results if i is not None]
+ 
+    # Is there a way for this to NOT involve a loop?????
+    if len(results) > 0:
+        for ii in range(len(results)):
+            if events_tmp is not None:
+                events_tmp = np.concatenate((events_tmp, results[ii][0]), axis=1)
+            else:
+                events_tmp = results[ii][0]
+
+        for ii in range(len(results)):
+            if blends_tmp is not None:
+                blends_tmp = np.concatenate((blends_tmp, results[ii][1]), axis=1)
+            else:
+                blends_tmp = results[ii][1]
+
+    t1 = time.time()
+
+    runtime = t1 - t0
+
+    return runtime

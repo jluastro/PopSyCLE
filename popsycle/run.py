8bf738b2228511ac017e315839c1326ef150cce2
Michael Medford <MichaelMedford@berkeley.edu>
2020-02-04 22:01:24 -0800

    add overwrite to perform_pop_syn from args

--- popsycle/run.py
+++ popsycle/run.py
@@ -179,6 +179,7 @@ def run():
             bin_edges_number=popsycle_config['bin_edges_number'],
             BH_kick_speed_mean=popsycle_config['BH_kick_speed_mean'],
             NS_kick_speed_mean=popsycle_config['NS_kick_speed_mean'],
+            overwrite=args.overwrite,
             seed=args.seed)
 
     if not args.skip_calc_events:

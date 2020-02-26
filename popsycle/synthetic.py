fix make_label_file calculation

popsycle/synthetic.py
@@ -2584,10 +2584,10 @@ def make_label_file(h5file_name, overwrite=False):
                 N_NS = 0
                 N_BH = 0
             else:
-                N_stars = len(np.where(dataset['popid'] == 0)[0])
-                N_WD = len(np.where(dataset['popid'] == 101)[0])
-                N_NS = len(np.where(dataset['popid'] == 102)[0])
-                N_BH = len(np.where(dataset['popid'] == 103)[0])
+                N_stars = np.sum(dataset['rem_id'] == 0)
+                N_WD = np.sum(dataset['rem_id'] == 101)
+                N_NS = np.sum(dataset['rem_id'] == 102)
+                N_BH = np.sum(dataset['rem_id'] == 103)
 
             data_dict['file_name'].append(dset_name)
             data_dict['long_start'].append(l_array[ll])
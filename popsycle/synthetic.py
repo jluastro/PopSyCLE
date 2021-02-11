a553748c2237c6d88315fc512f7b493a4eb583fc
Michael Medford <MichaelMedford@berkeley.edu>
2021-02-11 16:58:00 -0500

    add None object to multiplicity_list

--- popsycle/synthetic.py
+++ popsycle/synthetic.py
@@ -106,6 +106,7 @@ all_filt_list = ['ubv,U', 'ubv,B', 'ubv,V', 'ubv,I', 'ubv,R',
 # List of all supported multiplicity classes
 ##########
 multiplicity_list = {'None': None,
+                     None: None,
                      'ResolvedDK': MultiplicityResolvedDK}
 
 ###########################################################################

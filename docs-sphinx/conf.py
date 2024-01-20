875b869661c96152e1164d0c3cf887339c70fe7b
nsabrams <nsabrams@gmail.com>
2024-01-19 17:08:18 -0800

    added sphinx-rtd-theme to requirements and fixed conf.py

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -14,7 +14,7 @@ release = '2.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'numpydoc'] #'sphinx.ext.napoleon'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'numpydoc', 'sphinx_rtd_theme'] #'sphinx.ext.napoleon'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']

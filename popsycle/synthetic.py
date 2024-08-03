d05689034ea1bfc3cf9519b105b5931a33f1cad7
Kai Marshall <kaimarshallin@berkeley.edu>
2024-08-03 12:11:55 -0700

    Reduce to using 1 kdt in cands_radius (cartesian)

--- popsycle/synthetic.py
+++ popsycle/synthetic.py
@@ -4482,27 +4482,28 @@ def _calc_event_cands_radius(bigpatch, radius_cut, obs_time):
     startPosSph_lenses = np.asarray(start_movement_spherical_noCartesian(lenses[['rad','glat','glon']], lenses[['vx', 'vy', 'vz']], obs_time)).T
     
 
+    ########
     ## array of total displacements for lenses, all displacement is related to lenses
-    total_disp_arr = np.sqrt((startPosSph_lenses - endPosSph_lenses)[:, 1]**2 + (startPosSph_lenses - endPosSph_lenses)[:, 2]**2)
+    ## np.tan to convert to cartesian query
+    total_disp_arr = np.tan(np.sqrt((startPosSph_lenses - endPosSph_lenses)[:, 1]**2 + (startPosSph_lenses - endPosSph_lenses)[:, 2]**2))
+
     max_disp = total_disp_arr.max()
     disp_95 = np.percentile(total_disp_arr, 95)
     radius_cut_95 = 2*disp_95 ## double because lens and source could each have about this much motion 
     ## could have more, but that case is currently being neglected, as this will catch almost all cases (especially if taken from midpoint positions)
-    radius_cut_max = 2*max_disp ## in units of radians
+    radius_cut_max = 2*max_disp ## in cartesian
     print('====== displacements ======')    
-    print('max: %s' % max_disp)
-    print('95th percentile: %s' % disp_95)
+    print('max: %s (in cartesian)' % max_disp)
+    print('95th percentile: %s (in cartesian)' % disp_95)
     print('===========================')
 
-
     ## note: radius_cut is input with milliarcseconds, and maxSphRadius is currently in radians, so there needs to be a unit conversion
-    maxSphRadius = ((radius_cut * units.mas).to(units.radian)) / units.radian ## convert to radians (from mas)
-    print('DIAGNOSTIC: maxSphRadius/radius_cut in radians = %s' % maxSphRadius)
-    kdt = cKDTree(midPosSph_sources[:, 1:3]) ## now uses midpoint instead of start to capture more relevant potential events
-    kdt_cart = cKDTree(np.array(c.cartesian.xyz.T))
-    ## CHANGE: make a more efficient way to use only 1 kdtree
-    print('DIAGNOSTIC: number of objects (sources) in the kdtree: %d' % len(midPosSph_sources[:, 1:3]))
-    
+    maxSphRadius = np.tan((radius_cut * units.mas).to(units.radian)) ## convert to radians (from mas), then to cartesian w/ tan
+    print('DIAGNOSTIC: maxSphRadius/radius_cut in cartesian = %s' % maxSphRadius)
+    carts = np.array(c.cartesian.xyz.T)
+    kdt_cart = cKDTree(carts)
+    print('DIAGNOSTIC: number of objects (sources) in the kdtree: %d' % len(carts))
+
     ## in the case that displacements are larger, we will use the radius cut
     ## should usually be the same radius_cut_95 and radius_cut_max
     radius_cut_95 = min(radius_cut_95, maxSphRadius)
@@ -4514,19 +4515,21 @@ def _calc_event_cands_radius(bigpatch, radius_cut, obs_time):
     totalIsolatedObjects = 0
     ## this could be implemented dynamically such that for cases where there are lots of large values
     ## we take a percentile that will run very fast and do those, then use the radius_cut for the others
+    
     for i, lens in enumerate(lenses):
         
         if total_disp_arr[i] <= disp_95:
             ## if the ith lens has a displacement lower than disp_95, we use the 95th percentile threshold value
-            results = kdt.query_ball_point((midPosSph_lenses[i][1], midPosSph_lenses[i][2]), radius_cut_95)
-            ## lens' glat, glon (coords)
+            results = kdt_cart.query_ball_point(carts[i], radius_cut_95)
+            ## lens' x,y,z coords
             
         else: ##in the >95% case
-            results = kdt.query_ball_point((midPosSph_lenses[i][1], midPosSph_lenses[i][2]), radius_cut_max)
-        
-        results = [index for index in results if index != i] ## dont count the same object as a source if its already the lens
-        
-        for res in results:
+            results = kdt_cart.query_ball_point(carts[i], radius_cut_max)
+
+        ## dont count the same object as a source if its already the lens
+        results = [index for index in results if index != i] 
+       
+        for res in results: ## switch back to results after
             ## res,i are the indices of the sources, lenses (respectively) in the original patch list
             if(lens['rad'] < midPosSph_sources[res][0]):
                 # lens is nearer than the source
@@ -4536,7 +4539,148 @@ def _calc_event_cands_radius(bigpatch, radius_cut, obs_time):
                 
         if len(results) == 0:
             totalIsolatedObjects += 1
+
+    ########
+    ## INTERTWINED TESTING
+    ## holding on to below code for potential testing, delete later.
+    ########
+    ## np.tan to convert to cartesian query
+    """
+    total_disp_arr = np.tan(np.sqrt((startPosSph_lenses - endPosSph_lenses)[:, 1]**2 + (startPosSph_lenses - endPosSph_lenses)[:, 2]**2))
     
+    #### FOR TESTING ####
+    r_total_disp_arr = np.sqrt((startPosSph_lenses - endPosSph_lenses)[:, 1]**2 + (startPosSph_lenses - endPosSph_lenses)[:, 2]**2)
+    r_max_disp = r_total_disp_arr.max()
+    r_disp_95 = np.percentile(r_total_disp_arr, 95)
+    r_radius_cut_95 = 2*r_disp_95
+    r_radius_cut_max = 2*r_max_disp ## in units of radians
+    r_maxSphRadius = (radius_cut * units.mas).to(units.radian) / units.radian
+    kdt = cKDTree(midPosSph_sources[:, 1:3])
+    r_radius_cut_95 = min(r_radius_cut_95, r_maxSphRadius)
+    r_radius_cut_max = min(r_radius_cut_max, r_maxSphRadius)
+    r_totalNearbyObjects = 0
+    r_totalIsolatedObjects = 0
+    #####################
+
+    max_disp = total_disp_arr.max()
+    disp_95 = np.percentile(total_disp_arr, 95)
+    radius_cut_95 = 2*disp_95 ## double because lens and source could each have about this much motion 
+    ## could have more, but that case is currently being neglected, as this will catch almost all cases (especially if taken from midpoint positions)
+    radius_cut_max = 2*max_disp ## in cartesian
+    print('====== displacements ======')    
+    print('max: %s (in cartesian)' % max_disp)
+    print('95th percentile: %s (in cartesian)' % disp_95)
+    print('===========================')
+
+
+    ## note: radius_cut is input with milliarcseconds, and maxSphRadius is currently in radians, so there needs to be a unit conversion
+    maxSphRadius = np.tan((radius_cut * units.mas).to(units.radian)) ## convert to radians (from mas), then to cartesian w/ tan
+    print('DIAGNOSTIC: maxSphRadius/radius_cut in cartesian = %s' % maxSphRadius)
+    ## kdt = cKDTree(midPosSph_sources[:, 1:3]) ## now uses midpoint instead of start to capture more relevant potential events
+    carts = np.array(c.cartesian.xyz.T)
+    kdt_cart = cKDTree(carts)
+    ## CHANGE: make a more efficient way to use only 1 kdtree
+    ## print('DIAGNOSTIC: number of objects (sources) in the kdtree: %d' % len(midPosSph_sources[:, 1:3]))
+    print('DIAGNOSTIC: number of objects (sources) in the kdtree: %d' % len(carts))
+
+    ## in the case that displacements are larger, we will use the radius cut
+    ## should usually be the same radius_cut_95 and radius_cut_max
+    radius_cut_95 = min(radius_cut_95, maxSphRadius)
+    radius_cut_max = min(radius_cut_max, maxSphRadius)
+    lens_id = []
+    sorc_id = []
+
+    totalNearbyObjects = 0 ## counts the number of nearby (potential) sources near a lens
+    totalIsolatedObjects = 0
+    ## this could be implemented dynamically such that for cases where there are lots of large values
+    ## we take a percentile that will run very fast and do those, then use the radius_cut for the others
+
+    def unshared(l1, l2):
+        ## assuming l1 & l2 are not the same length
+        unique_ls = []
+        if len(l1) > len(l2):
+            for i in l1:
+                if i not in l2:
+                    unique_ls.append(i)
+        if len(l2) > len(l1):
+            for i in l2:
+                if i not in l1:
+                    unique_ls.append(i)
+        return unique_ls
+    
+    def dist_cart(target, obj_carts):
+        print('target is', target)
+        print('obj_cart is', obj_carts)
+        return np.sqrt((target[0]-obj_carts[0])**2 + (target[1]-obj_carts[1])**2 + (target[2]-obj_carts[2])**2)
+    
+    def dist_rad(target, obj_rad):
+        print('target is', target)
+        print('obj_rad is', obj_rad)
+        return np.sqrt((target[1]-obj_rad[1])**2 + (target[2]-obj_rad[2])**2)
+    
+    for i, lens in enumerate(lenses):
+        
+        if total_disp_arr[i] <= disp_95:
+            ## if the ith lens has a displacement lower than disp_95, we use the 95th percentile threshold value
+            results = kdt_cart.query_ball_point(carts[i], radius_cut_95)
+            curr_cut = radius_cut_95
+            ## lens' glat, glon (coords)
+            
+        else: ##in the >95% case
+            results = kdt_cart.query_ball_point(carts[i], radius_cut_max)
+            curr_cut = radius_cut_max
+        
+        if r_total_disp_arr[i] <= r_disp_95:
+            r_results = kdt.query_ball_point((midPosSph_lenses[i][1], midPosSph_lenses[i][2]), r_radius_cut_95)
+            r_curr_cut = r_radius_cut_95
+
+        else:
+            r_results = kdt.query_ball_point((midPosSph_lenses[i][1], midPosSph_lenses[i][2]), r_radius_cut_max)
+            r_curr_cut = r_radius_cut_max
+
+        results = [index for index in results if index != i] ## dont count the same object as a source if its already the lens
+        r_results = [index for index in r_results if index != i]
+
+        if len(results) != len(r_results):
+            print ('============')
+            print('results are', results)
+            print('r_results are', r_results)
+            unshared_ind = unshared(results, r_results)
+            print('unshared index is', unshared_ind)
+            cart_dist = dist_cart(carts[i], carts[unshared_ind][0])
+            print('distance (cartesian) between lens and source is', cart_dist)
+            print('for ref (cart), search radius is', curr_cut)
+            print('cart dist is less than search?:', cart_dist < curr_cut)
+            ### after testing, delete curr_cut and all this stuff
+            rad_dist = dist_rad(midPosSph_lenses[i], midPosSph_sources[unshared_ind][0])
+            print('distance (radians) between lens and source is', rad_dist)
+            print('for ref (rad), search radius is', r_curr_cut)
+            print('radian dist is less than search?:', rad_dist < r_curr_cut)
+
+        for res in results: ## switch back to results after
+            ## res,i are the indices of the sources, lenses (respectively) in the original patch list
+            if(lens['rad'] < midPosSph_sources[res][0]):
+                # lens is nearer than the source
+                totalNearbyObjects += 1
+                lens_id.append(i) ## this is the index of the object in the original patch list
+                sorc_id.append(res) ## this is the index of the object in the original patch list
+
+        for res in r_results: ## switch back to results after
+            ## res,i are the indices of the sources, lenses (respectively) in the original patch list
+            if(lens['rad'] < midPosSph_sources[res][0]):
+                # lens is nearer than the source
+                r_totalNearbyObjects += 1
+                
+        if len(results) == 0:
+            totalIsolatedObjects += 1
+        
+        if len(r_results) == 0:
+            r_totalIsolatedObjects += 1
+    """
+    ########
+    ## END TESTING
+    ########
+
     ## convert to arrays
     sorc_id = np.array(sorc_id)
     lens_id = np.array(lens_id)

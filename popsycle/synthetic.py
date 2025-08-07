715ec0afa0427541d991174e089666af8d1eb6a4
Sage Remulla <shremull@berkeley.edu>
2025-08-06 17:25:00 -0700

    corrected refine_events .fits file name

--- popsycle/synthetic.py
+++ popsycle/synthetic.py
@@ -4219,8 +4219,8 @@ def _check_refine_events(input_root,filter_dict, red_law, overwrite,
     if not isinstance(input_root, str):
         raise Exception('input_root (%s) must be a string.' % str(input_root))
     
-    if not isinstance(filters_dict, dict):
-        raise Exception('filters_dict (%s) must be a dictionary.' % str(filters_dict))
+    if not isinstance(filter_dict, dict):
+        raise Exception('filter_dict (%s) must be a dictionary.' % str(filter_dict))
 
     if not isinstance(red_law, str):
         raise Exception('red_law (%s) must be a string.' % str(red_law))
@@ -4243,7 +4243,7 @@ def _check_refine_events(input_root,filter_dict, red_law, overwrite,
             raise Exception('seed (%s) must be None or an integer.' % str(seed))
 
     # Check to see that the filter dictionary, and red_law are valid
-    for system in filters_dict:
+    for system in filter_dict:
         if system not in photometric_system_dict:
             exception_str = 'photometric_system must be a key in ' \
                             'photometric_system_dict. \n' \
@@ -4253,7 +4253,7 @@ def _check_refine_events(input_root,filter_dict, red_law, overwrite,
             exception_str = exception_str[:-2]
             raise Exception(exception_str)
 
-        for filt in filters_dict[system]:
+        for filt in filter_dict[system]:
             if filt not in photometric_system_dict[system]:
                 exception_str = 'filter_name must be a value in ' \
                                 'photometric_system_dict[%s]. \n' \
@@ -4263,15 +4263,15 @@ def _check_refine_events(input_root,filter_dict, red_law, overwrite,
                 exception_str = exception_str[:-2]
                 raise Exception(exception_str)
             
-    key = photometric_system + '_' + filter_name
-    if red_law not in filt_dict[key]:
-        exception_str = 'red_law must be a value in ' \
-                        'filt_dict[%s]. \n' \
-                        'Acceptable values are : ' % key
-        for red_law in filt_dict[key]:
-            exception_str += '%s, ' % red_law
-        exception_str = exception_str[:-2]
-        raise Exception(exception_str)
+            key = system + '_' + filt
+            if red_law not in filt_dict[key]:
+                exception_str = 'red_law must be a value in ' \
+                                'filt_dict[%s]. \n' \
+                                'Acceptable values are : ' % key
+                for red_law in filt_dict[key]:
+                    exception_str += '%s, ' % red_law
+                exception_str = exception_str[:-2]
+                raise Exception(exception_str)
 
 
 def refine_events(input_root, filter_dict, red_law,
@@ -4348,10 +4348,10 @@ def refine_events(input_root, filter_dict, red_law,
     _check_refine_events(input_root, filter_dict, red_law,
                          overwrite, output_file, hdf5_file_comp, legacy, seed)
     
-    filters_string = '_'.join(['_'.join([system, '_'.join(filts)]) for system , filts in filters_dict.items()])
+    filters_string = '_'.join(['_'.join([system, '_'.join(filts)]) for system , filts in filter_dict.items()])
     if output_file == 'default':
         output_file = '{0:s}_refined_events_{1:s}_{2:s}.fits'.format(input_root, 
-                                                                     output_filters_string,
+                                                                     filters_string,
                                                                      red_law)
 
     t_0 = time.time()
@@ -4384,15 +4384,15 @@ def refine_events(input_root, filter_dict, red_law,
         
 
     # If photometric fields contain -99, convert to nan
-    photometric_system = filters_dict.keys()
-    filter_name = filters_dict.values()
+    photometric_system = filter_dict.keys()
+    filter_name = filter_dict.values()
     for system in photometric_system:
-        _convert_photometric_99_to_nan(event_tab, photometric_system)
-        _convert_photometric_99_to_nan(blend_tab, photometric_system)
+        _convert_photometric_99_to_nan(event_tab, system)
+        _convert_photometric_99_to_nan(blend_tab, system)
 
     # Only keep events with luminous sources
-    for system in filters_dict:
-        for filters in filters_dict[system]:
+    for system in filter_dict:
+        for filters in filter_dict[system]:
             event_tab = event_tab[~np.isnan(event_tab[system + '_' + filters + '_S'])]
 
     # Grab the obs_time from the calc_events log
@@ -4602,7 +4602,7 @@ def refine_events(input_root, filter_dict, red_law,
 
     line0 = 'FUNCTION INPUT PARAMETERS' + '\n'
     line1 = 'input_root : ' + input_root + '\n'
-    line2 = 'filter_dict : ' + ', '.join([': '.join([system, ', '.join(filts)]) for system , filts in filters_dict.items()]) + '\n'
+    line2 = 'filter_dict : ' + ', '.join([': '.join([system, ', '.join(filts)]) for system , filts in filter_dict.items()]) + '\n'
     line3 = 'red_law : ' + red_law + '\n'
 
     line4 = 'VERSION INFORMATION' + '\n'
@@ -4623,13 +4623,12 @@ def refine_events(input_root, filter_dict, red_law,
         if len(companion_table) > 0:
             line14 = output_file[:-5] + "_companions.fits" + ' : companions refined events'
     
-    for system in filter_dict:
-        for filters in filter_dict[system]:
-            with open(input_root + '_refined_events_' + system + '_' + filters + '_' + red_law + '.log', 'w') as out:
-                out.writelines([line0, dash_line, line1, line2, line3, empty_line,
-                                line4, dash_line, line5, line6, line7, empty_line,
-                                line8, dash_line, line9, line10, line11, empty_line,
-                                line12, dash_line, line13, line14])
+
+    with open(input_root + '_refined_events_' + filters_string + '_' + red_law + '.log', 'w') as out:
+        out.writelines([line0, dash_line, line1, line2, line3, empty_line,
+                        line4, dash_line, line5, line6, line7, empty_line,
+                        line8, dash_line, line9, line10, line11, empty_line,
+                        line12, dash_line, line13, line14])
 
     print('refine_events runtime : {0:f} s'.format(t_1 - t_0))
     return

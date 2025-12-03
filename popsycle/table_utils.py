830574c1c59e11032076a3e3e875141a8d118d50
nsabrams <nsabrams@gmail.com>
2025-12-02 20:14:07 -0800

    moved cut_Mruns to table utils

--- popsycle/table_utils.py
+++ popsycle/table_utils.py
@@ -64,3 +64,81 @@ def trim_popsycle_tables(idx, event_tab, comps_tab, lcurv_tab):
     lcurv_tab_t = Table.from_pandas(lcurv_df_t)
     
     return event_tab_t, comps_tab_t, lcurv_tab_t
+
+
+def cut_Mruns(t_prim, t_comp_rb, t_comp_rb_mp, min_mag, delta_m_cut, u0_cut, photometric_system, filter_name, S_LSN):
+    """
+    Make observational cuts on PopSyCLE runs with multiple systems
+
+    Parameters
+    ----------
+    t_prim : Astropy table
+        Events table from refine_binary_events.
+        Must contain 'observable_n_peaks' column.
+
+    t_comp_rb : Astropy table
+        Companion table from refine_binary_events.
+
+    t_comp_rb_mp : Astropy table
+        Multi peak table from refine binary events 
+        (each row corresponds to a peak in a lightcurve).
+
+    min_mag : float
+        Minimum baseline or source magnitude (specified by S_LSN).
+
+    delta_m_cut : float or None.
+        Minimum bump magnitude.
+
+    u0_cut : float
+        Maximum u0.
+
+    photometric_system : str
+        Photometric system when cutting on min_mag.
+
+    filter_name : str
+        Filter name used when cutting on min_mag and delta_m_cut.
+
+    S_LSN : str
+        'S' for source mag cut or 'LSN' for baseline magnitude cut.
+
+    Returns
+    -------
+    t_both_mcut : Astropy table
+        Table with specified observational cuts.
+        
+    t_both_mcut_one_peak : Astropy table
+        Table with specified observational cuts and only single peaked events.
+        
+    t_multiples_mcut_multi_peak : Astropy table
+        Table with specified observational cuts and only multipeaked events
+        containing a multiple system.
+    """
+    #S_LSN is source or baseline mag cut
+    if S_LSN == 'S':
+        mag_cut = t_prim['{}_{}_app_S'.format(photometric_system, filter_name)] <= min_mag
+    elif S_LSN == 'LSN':
+        mag_cut = t_prim['{}_{}_app_LSN'.format(photometric_system, filter_name)] <= min_mag
+    
+    u0_cut = np.abs(t_prim['u0']) < u0_cut
+    
+    binary_filt = (t_prim['isMultiple_L'] == 1) | (t_prim['isMultiple_S'] == 1)
+    single_filt = (t_prim['isMultiple_L'] == 0) & (t_prim['isMultiple_S'] == 0)
+    assert(len(t_prim) == (sum(binary_filt) + sum(single_filt)))
+
+    if delta_m_cut is not None:
+        delta_m_cut = ((t_prim['bin_delta_m'] > 0.1) & binary_filt) | ((t_prim['delta_m_{}'.format(filter_name)] > 0.1) & single_filt)
+        total_cut = mag_cut & u0_cut & delta_m_cut
+    else:
+        total_cut = mag_cut & u0_cut
+
+    t_both_mcut = t_prim[total_cut]
+    binary_filt_cut = (t_both_mcut['isMultiple_L'] == 1) | (t_both_mcut['isMultiple_S'] == 1)
+    single_filt_cut = (t_both_mcut['isMultiple_L'] == 0) & (t_both_mcut['isMultiple_S'] == 0)
+    assert(len(t_both_mcut) == (sum(binary_filt_cut) + sum(single_filt_cut)))
+
+    t_mult_mcut_no_peaks = t_both_mcut[binary_filt_cut & (t_both_mcut['observable_n_peaks'] == 0)] 
+    t_both_mcut_one_peak = t_both_mcut[(binary_filt_cut & (t_both_mcut['observable_n_peaks'] == 1)) | single_filt_cut]
+    t_multiples_mcut_multi_peak = t_both_mcut[binary_filt_cut & (t_both_mcut['observable_n_peaks'] > 1)]
+    assert(len(t_both_mcut) == (len(t_mult_mcut_no_peaks) + len(t_both_mcut_one_peak) + len(t_multiples_mcut_multi_peak)))
+
+    return t_both_mcut, t_both_mcut_one_peak, t_multiples_mcut_multi_peak
